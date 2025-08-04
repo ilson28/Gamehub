40179e8133c30a7cff6c3089a078e655caf79301
ilson28 <ilsodiazmorelo28@gmail.com>
2025-08-04 15:53:42 -0500

    feat(transaccionService): add transaccionService and endpoints to get total actives transaccions and to get today's sales.

---
+++ frontend/gamehub/src/services/transaccionService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8080/api/transacciones';
+
+export const getTotalActiveTransaccions = async () => {
+
+    const response = await axios.get(`${API_URL}/total-active-transaccions`);
+    return response.data;
+}
+
+export const totalSalesToday = async () => {
+    const response = await axios.get(`${API_URL}/total-sales-today`);
+    return response.data;
+}
