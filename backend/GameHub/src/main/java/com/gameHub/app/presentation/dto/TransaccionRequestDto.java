9da76c1efc1138e0c2dcda01786d26342d76ee0b
ilson28 <ilsodiazmorelo28@gmail.com>
2025-08-05 17:43:29 -0500

    refactor(transaccionRequestDto): remove unused NotNull validation annotation

--- backend/GameHub/src/main/java/com/gameHub/app/presentation/dto/TransaccionRequestDto.java
+++ backend/GameHub/src/main/java/com/gameHub/app/presentation/dto/TransaccionRequestDto.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
