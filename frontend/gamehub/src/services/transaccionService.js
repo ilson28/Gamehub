9d5b96daee1bd7fe1b7d810bb5e6630630c63220
ilson28 <ilsodiazmorelo28@gmail.com>
2025-09-09 16:52:31 -0500

    refactor(transaccionService): add functions to fetch transactions by type and rental status

--- frontend/gamehub/src/services/transaccionService.js
+++ frontend/gamehub/src/services/transaccionService.js
@@ -11,3 +11,17 @@ export const getTotalSalesToday = async () => {
     const response = await axios.get(`${API_URL}/total-sales-today`);
     return response.data;
 }
+
+export const getTransactionsForType = async (type, date) => {
+
+    const response = await axios.get(`${API_URL}/type/${type}/${date}`);
+    return response.data;
+
+}
+
+export const getRentalTransactions = async (estado, date) => {
+
+    const response = await axios.get(`${API_URL}/alquiler/${estado}/${date}`);
+    return response.data;
+
+}
