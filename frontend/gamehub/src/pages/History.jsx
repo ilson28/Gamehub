a33c83534a156cbd396107de03c1f65d045464ae
ilson28 <ilsodiazmorelo28@gmail.com>
2025-09-09 12:55:13 -0500

    refactor(History): Adjust styles and add a table to display sales and rental history

--- frontend/gamehub/src/pages/History.jsx
+++ frontend/gamehub/src/pages/History.jsx
@@ -25,7 +25,7 @@ const History = () => {
                 <span className="text-gray-600">Consulta el historial completo de ventas y alquileres</span>
             </div>
 
-            <div className="p-6 flex flex-col gap-8 rounded-lg bg-white">
+            <div className="p-6 flex flex-col gap-5 rounded-lg bg-white">
 
                 <div className="flex gap-10  border-b border-gray-200">
 
@@ -60,15 +60,15 @@ const History = () => {
 
                     {tabActive === "sale" ?
 
-                        <div className="flex items-center gap-3 text-blue-700">
-                            <FaCartShopping />
+                        <div className="flex items-center gap-3 text-black font-semibold">
+                            <FaCartShopping color="blue" />
                             <span>Historial de Ventas</span>
                         </div>
 
                         :
 
-                        <div className="flex items-center gap-3 text-blue-700">
-                            <IoTimeSharp />
+                        <div className="flex items-center gap-3 text-black font-semibold">
+                            <IoTimeSharp color="blue" />
                             <span>Historial de Ventas</span>
                         </div>
 
@@ -87,10 +87,59 @@ const History = () => {
                         <input
                             type="date"
                             className="p-2 outline-none focus:border-blue-800 rounded-md focus:border-2 border border-gray-300" />
-
-
                     </div>
                 </div>
+
+                <div className="overflow-x-auto">
+                    <table className="w-full text-left">
+
+                        <thead className="bg-gray-100 text-gray-600 ">
+                            <tr>
+                                <th className="pl-8">Fecha y Hora </th>
+                                <th>Cédula Cliente</th>
+                                <th>Tipo</th>
+                                <th>Total</th>
+                                <th>Fecha Devolución</th>
+                                <th>Estado</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div className="flex flex-col gap-1 pl-8 text-sm">
+                                        <span>13/07/2025</span>
+                                        <span className="text-gray-500">16:20</span>
+                                    </div>
+                                </td>
+                                <td>11223344</td>
+                                <td><span className="bg-blue-200 text-blue-900 font-medium rounded-xl px-2">alquiler</span></td>
+                                <td>$15.000</td>
+                                <td>20/07/2025</td>
+                                <td>activo</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div className="flex flex-col gap-1 pl-8 text-sm">
+                                        <span>13/07/2025</span>
+                                        <span className="text-gray-500">16:20</span>
+                                    </div>
+                                </td>
+                                <td>11223344</td>
+                                <td><span className="bg-blue-200 text-blue-900 font-medium rounded-xl px-2">alquiler</span></td>
+                                <td>$15.000</td>
+                                <td>20/07/2025</td>
+                                <td>activo</td>
+                                <td></td>
+                            </tr>
+
+                        </tbody>
+
+                    </table>
+                </div>
+
             </div>
 
         </div >
