6f5c38328f2abbc58c8acd960562b7172b28403b
ilson28 <ilsodiazmorelo28@gmail.com>
2025-10-03 19:35:00 -0500

    feat(Cart): Improve cart design and add transaction options (sale/rental)

--- frontend/gamehub/src/pages/Cart.jsx
+++ frontend/gamehub/src/pages/Cart.jsx
@@ -13,27 +13,46 @@ const Cart = () => {
             <h1 className="text-gray-900 font-bold text-3xl">Carrito de Compras</h1>
             <span className="text-gray-500">Revisa tus videojuegos y completa tu transacción</span>
         </div>
+        <div className="flex flex-col gap-5 md:flex-row">
 
-        <div className="bg-white border border-gray-200 rounded-b-sm">
+            <div className="bg-white border border-gray-200 rounded-b-sm grow">
 
-            <div className="p-5 border-b border-b-gray-200 text-xl font-semibold text-gray-900">
-                <p>Videojuegos en el carrito</p>
+                <div className="p-5 border-b border-b-gray-200 text-xl font-medium text-gray-900">
+                    <h2>Videojuegos en el carrito</h2>
+                </div>
+                {
+                    games.map(game => (<div key={game.id} className="p-4 border-b border-b-gray-200">
+                        <Videojuego
+                            id={game.id}
+                            title={game.titulo}
+                            genre={game.genero}
+                            platform={game.plataforma}
+                            sale={game.precioVenta}
+                            rent={game.precioAlquiler}
+                            stock={game.stock}
+                            img={game.imagenUrl}
+                            cart />
+                    </div>))
+                }
+
             </div>
-            {
-                games.map(game => (<div key={game.id} className="p-4 border-b border-b-gray-200">
-                    <Videojuego
-                        id={game.id}
-                        title={game.titulo}
-                        genre={game.genero}
-                        platform={game.plataforma}
-                        sale={game.precioVenta}
-                        rent={game.precioAlquiler}
-                        stock={game.stock}
-                        img={game.imagenUrl}
-                        cart />
-                </div>))
-            }
 
+            <div className="bg-white border border-gray-200 rounded-b-sm grow">
+                <div className="p-5 text-md md:text-xl font-medium text-gray-900">
+                    <h2>Tipo de transacción</h2>
+                </div>
+                <div className="px-5 flex flex-col gap-3 ">
+                    <div className="flex items-center gap-3 border-2 border-gray-300 p-3 rounded-md">
+                        <input type="radio" name="type" defaultChecked id="sale" />
+                        <label htmlFor="sale">Venta</label>
+                    </div>
+                    <div className="flex items-center gap-3 border-2 border-gray-300 p-3 rounded-md">
+                        <input type="radio" name="type" id="rent" />
+                        <label htmlFor="rent">Alquiler</label>
+                    </div>
+
+                </div>
+            </div>
         </div>
 
 
