416da7fd9bccc559d6f7385d808cbe0f4353ac01
ilson28 <ilsodiazmorelo28@gmail.com>
2025-10-15 09:29:28 -0500

    feat(ClienteResponseDto): create ClienteResponseDto class to represent client data in responses.

---
+++ backend/GameHub/src/main/java/com/gameHub/app/service/implementation/ClienteResponseDto.java
@@ -0,0 +1,15 @@
+package com.gameHub.app.service.implementation;
+import com.gameHub.app.util.enums.Role;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ClienteResponseDto {
+
+    private Integer cedula;
+    private String nombre, apellido, direccion, telefono, sexo;
+    private Role role;
+}
