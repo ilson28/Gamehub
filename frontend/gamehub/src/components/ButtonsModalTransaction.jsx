6d36248d45202470e20325297b9453724ced7f4e
ilson28 <ilsodiazmorelo28@gmail.com>
2025-10-23 22:32:24 -0500

    feat(ButtonsModalTransaction): add buttons component for transaction confirmation and cancellation

---
+++ frontend/gamehub/src/components/ButtonsModalTransaction.jsx
@@ -0,0 +1,45 @@
+import { VscLoading } from "react-icons/vsc";
+import useModalContext from "../hooks/useModalContext";
+import ButtonCard from "./ButtonCard";
+
+
+const ButtonsModalTransaction = ({
+    onClick,
+    loading,
+    transactionState = ""
+}) => {
+
+    const { setState } = useModalContext();
+    return (
+        <div className="m-6 flex flex-col sm:flex-row gap-2">
+            <ButtonCard
+                disabled={loading}
+                onClick={() => setState(false)}
+                className="w-full border border-gray-400 text-gray-700">
+                Cancelar
+            </ButtonCard>
+            {
+                transactionState !== "devuelto" &&
+                <ButtonCard
+                    disabled={loading}
+                    color="blue"
+                    className="w-full"
+                    onClick={onClick}
+                >
+                    {
+                        loading ? (<div className="flex justify-center items-center gap-2">
+                            <VscLoading className="animate-spin text-2xl" /> Cargando...
+                        </div>)
+                            :
+                            (
+                                transactionState ?
+                                    "Realizar Devolucion" :
+                                    "Confirmar transaccion"
+                            )
+                    }
+                </ButtonCard>}
+        </div>
+    )
+}
+
+export default ButtonsModalTransaction
