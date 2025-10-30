08b6fbc5fbcbe529123f366ebe7bc0aa3ad6b5d1
ilson28 <ilsodiazmorelo28@gmail.com>
2025-10-30 10:28:07 -0500

    feat(VideoJuegoRepository): add new query method to update stock by game ID.

--- backend/GameHub/src/main/java/com/gameHub/app/persistence/repository/VideoJuegoRepository.java
+++ backend/GameHub/src/main/java/com/gameHub/app/persistence/repository/VideoJuegoRepository.java
@@ -1,6 +1,7 @@
 package com.gameHub.app.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.gameHub.app.persistence.entity.VideoJuego;
@@ -18,4 +19,9 @@ public interface VideoJuegoRepository extends JpaRepository<VideoJuego, Integer>
 
     @Query("SELECT COALESCE(SUM(v.stock), 0) FROM VideoJuego v where v.activo = 1")
     Long TotalStock();
+
+    @Modifying
+    @Query("UPDATE VideoJuego v SET v.stock = :newStock WHERE v.id = :id")
+    int updateStock(int id, int newStock);
+
 }
