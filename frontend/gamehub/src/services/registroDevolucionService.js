333fd5607c926029584b0378a7d1a7284ce4d8d6
ilson28 <ilsodiazmorelo28@gmail.com>
2025-11-24 17:50:16 -0500

    feat(RegistroDevolucionService): update getAll method to include pagination and filtering parameters

--- frontend/gamehub/src/services/registroDevolucionService.js
+++ frontend/gamehub/src/services/registroDevolucionService.js
@@ -1,25 +1,36 @@
 import axios from 'axios';
 const API_URL = 'http://localhost:8080/api/registro-devoluciones';
 
-const getFilterRegistroDevoluciones = async (cliente, fromDate, toDate) => {
-    const params = new URLSearchParams();
+// const getAll = async (cliente, fromDate, toDate) => {
+//     const params = new URLSearchParams();
 
-    if (cliente) params.append("cliente", cliente);
-    if (fromDate) params.append("fromDate", fromDate);
-    if (toDate) params.append("toDate", toDate);
+//     if (cliente) params.append("cliente", cliente);
+//     if (fromDate) params.append("fromDate", fromDate);
+//     if (toDate) params.append("toDate", toDate);
 
-    const response = await axios.get(`${API_URL}?${params.toString()}`);
+//     const response = await axios.get(`${API_URL}?${params.toString()}`);
+//     return response.data;
+// };
+const getAll = async (cliente, fromDate, toDate, page=0) => {
+   
+
+    const response = await axios.get(`${API_URL}`,{
+        params: {
+            cliente,
+            fromDate,
+            toDate,
+            size:2,
+            page
+        }
+     });
     return response.data;
 };
 
-const getAll = async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
-}
+
 
 const create = async (registroDevolucion) => {
     const response = await axios.post(API_URL, registroDevolucion);
     return response.data;
 }
 
-export { getAll, getFilterRegistroDevoluciones, create };
+export { getAll, create };
