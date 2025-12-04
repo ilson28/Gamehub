a1ca0aafe4a7989a944f620d7cf943b32f125dfc
ilson28 <ilsodiazmorelo28@gmail.com>
2025-12-03 19:46:04 -0500

    feat(RegistroDevolucionRepository): update query to cast cedula as string for filtering

--- backend/GameHub/src/main/java/com/gameHub/app/persistence/repository/RegistroDevolucionRepository.java
+++ backend/GameHub/src/main/java/com/gameHub/app/persistence/repository/RegistroDevolucionRepository.java
@@ -11,15 +11,13 @@ import com.gameHub.app.persistence.entity.RegistroDevolucion;
 
 public interface RegistroDevolucionRepository extends JpaRepository<RegistroDevolucion, Integer> {
 
-        @Query("""
-                        SELECT r
-                        FROM RegistroDevolucion r
-                        JOIN r.transaccion t
-                        JOIN t.cliente c
-                        WHERE (:cedula IS NULL OR c.cedula LIKE %:cedula%)
-                        AND (:fromDate IS NULL OR r.fecha >= :fromDate)
-                        AND (:toDate IS NULL OR r.fecha <= :toDate)
-                        """)
+        @Query("SELECT r " +
+                        "FROM RegistroDevolucion r " +
+                        "JOIN r.transaccion t " +
+                        "JOIN t.cliente c " +
+                        "WHERE (:cedula IS NULL OR CAST(c.cedula AS string) LIKE CONCAT('%', :cedula, '%')) " +
+                        "AND (:fromDate IS NULL OR r.fecha >= :fromDate) " +
+                        "AND (:toDate IS NULL OR r.fecha <= :toDate)")
         Page<RegistroDevolucion> findAllWithFilters(
                         @Param("cedula") String cedula,
                         @Param("fromDate") LocalDate fromDate,
