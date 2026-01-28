5f71df69c88b9a2ea427d115f84cbc18a7d3fc25
ilson28 <ilsodiazmorelo28@gmail.com>
2026-01-28 16:07:48 -0500

    feat(frontend): comment out ReactQueryDevtools in main.jsx

--- frontend/gamehub/src/main.jsx
+++ frontend/gamehub/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 //  Crear una instancia de QueryClient
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
 
   </StrictMode>,
